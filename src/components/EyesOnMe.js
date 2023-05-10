687f666ff0c8239570b97d1377ffe9a5796dd9f3
Eduardo Heyer <eduardo.heyer12@gmail.com>
2023-05-10 13:03:29 -0400

    completed react lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function Focus() {
+        console.log("Good!")
+    }
+
+    function Blur() {
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <div>
+            <button onFocus={Focus} onBlur={Blur}>Eyes on me</button>
+        </div>
+    )
+}
+
+
+
+// On that button, add event handlers that listen for the focus and blur events.
+// When the focus event fires, use console.log to print out the text 'Good!'.
+// When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'.
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,24 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleKeypad() {
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleKeypad}>
+            </input>
+        </div>
     )
 }
+
+// We'll keep things super simple instead, and use an <input type="password" /> field to capture input. Here's how to complete the exercise:
+
+// In the components/Keypad.js file, create a Keypad React component.
+// In that component, render an input with the right type.
+// On that input, add an event handler that listens for the change event.
+// When that event fires, use console.log to print out the text 'Entering password...'.
+
 
 export default Keypad;
